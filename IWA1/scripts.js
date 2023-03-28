a3a60c112d124fb03c3eda09ae8b2371bceddfe0
Ndimande <mkatroso.deep@gmail.com>
2023-03-28 22:20:01 +0200

    IWA2

--- IWA1/scripts.js
+++ IWA1/scripts.js
@@ -35,3 +35,13 @@ if (newValue >= MAX_NUMBER) {
 
 subtract.addEventListener('click', subtractHandler)
 add.addEventListener('click', addHandler)
+
+/* This is the firstname of the user /*
+
+const user = 'John'
+
+
+/* This is the lastname of the user /* const surname = 'Smith'
+
+
+console.log(user; surname;)
